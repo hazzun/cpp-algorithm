31c6d1b7f1d7908abd1a93f2c8214950a894959f
hajoon <gkwns0516@gmail.com>
2023-09-03 01:29:11 +0900

    [BFS] 코드는 맞는데 메모리초과.. 어케수정하누

--- code_tree/bfs/places-can-go.cpp
+++ code_tree/bfs/places-can-go.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <utility>
 
@@ -7,55 +6,60 @@ using namespace std;
 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+int board[102][102];
+bool vis[102][102];
 
-int vis[102][102];
+int n, k;
+queue<pair<int, int>> Q;
 
-int solution(vector<vector<int>> maps)
+void bfs(int x, int y)
 {
-  int row = maps.size();
-  int col = maps[0].size();
-
-  for (int i = 0; i < row; i++)
-  { // 행
-    for (int j = 0; j < col; j++)
-    { // 열
-      if (maps[i][j] == 0)
-        vis[i][j] = -1;
-    }
-  }
-
-  queue<pair<int, int>> Q;
-  Q.push({0, 0});
-  vis[0][0] = 1;
-
-  bool isEscape = false;
-
+  Q.push({x, y});
   while (!Q.empty())
   {
     auto cur = Q.front();
     Q.pop();
+    vis[cur.first][cur.second] = 1;
 
     for (int dir = 0; dir < 4; dir++)
     {
       int nx = cur.first + dx[dir];
       int ny = cur.second + dy[dir];
 
-      if (nx < 0 || ny < 0 || nx >= row || ny >= col)
+      if (board[nx][ny] == 1 || vis[nx][ny])
         continue;
-      if (maps[nx][ny] == 0 || vis[nx][ny] > 0)
+      if (nx < 0 || nx >= n || ny < 0 || ny >= n)
         continue;
-
-      vis[nx][ny] = vis[cur.first][cur.second] + 1;
-      if (nx == row - 1 && ny == col - 1)
-      {
-        isEscape = true;
-        // break;
-      }
       Q.push({nx, ny});
     }
   }
-  if (isEscape)
-    return vis[row - 1][col - 1];
-  else
-    return -1;
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n >> k;
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      cin >> board[i][j];
+
+  while (k--)
+  {
+    int x, y;
+    cin >> x >> y;
+    board[x - 1][y - 1] = 1;
+    bfs(x - 1, y - 1);
+  }
+
+  int count = 0;
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      if (vis[i][j])
+        count++;
+
+  cout << count;
 }
