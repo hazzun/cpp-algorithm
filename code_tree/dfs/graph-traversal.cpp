d787dcc2eff7c93f0a650117bde8b71c710301ea
hajoon <gkwns0516@gmail.com>
2023-09-05 23:34:23 +0900

    [DFS] 그래프탐색 - 인접리스트로 풀기

--- code_tree/dfs/graph-traversal.cpp
+++ code_tree/dfs/graph-traversal.cpp
@@ -1,39 +1,96 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int board[1002][1002];
+// bool vis[1002];
+// int n, m;
+// int cnt;
+
+// void dfs(int start)
+// {
+//   for (int i = 0; i < n; i++)
+//   {
+//     if (board[start][i] == 1 && !vis[i])
+//     {
+//       vis[i] = true;
+//       cnt++;
+//       dfs(i);
+//     }
+//   }
+// }
+
+// int main()
+// {
+//   ios::sync_with_stdio(0);
+//   cin.tie(0);
+
+//   cin >> n >> m;
+//   while (m--)
+//   {
+//     int x, y;
+//     cin >> x >> y;
+//     board[x - 1][y - 1] = 1;
+//     board[y - 1][x - 1] = 1;
+//   }
+//   dfs(0);
+//   if (cnt)
+//     cout << cnt - 1;
+//   else
+//     cout << 0;
+// }
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int board[1002][1002];
+int n, m, cnt;
+vector<int> v[1002];
 bool vis[1002];
-int n, m;
-int cnt;
 
-void dfs(int start)
+void dfs(int node)
 {
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < v[node].size(); i++)
   {
-    if (board[start][i] == 1 && !vis[i])
+    if (!vis[v[node][i]])
     {
-      vis[i] = true;
+      vis[v[node][i]] = true;
       cnt++;
-      dfs(i);
+      cout << "node : " << node << ", v[node][i] : " << v[node][i] << "\n";
+      dfs(v[node][i]);
     }
   }
 }
 
 int main()
 {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
   cin >> n >> m;
+
+  int x, y;
   while (m--)
   {
-    int x, y;
     cin >> x >> y;
-    board[x - 1][y - 1] = 1;
-    board[y - 1][x - 1] = 1;
+
+    v[x].push_back(y);
+    v[y].push_back(x);
   }
-  dfs(0);
-  if (cnt)
-    cout << cnt - 1;
-  else
-    cout << 0;
+  for (int i = 1; i <= n; i++)
+  {
+    cout << i << " : ";
+    for (int j = 0; j < v[i].size(); j++)
+    {
+      cout << v[i][j] << " ";
+    }
+    cout << "\n";
+  }
+  cout << "\n";
+  vis[1] = true;
+  dfs(1);
+
+  cout << cnt;
 }
