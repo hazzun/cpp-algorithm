2b67968ec4ce96464a1c4ac046e6ce5860b86b28
hajoon <87131215+hazzun@users.noreply.github.com>
2023-09-21 16:18:09 +0900

    [Silver III] Title: N과 M (3), Time: 364 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+
+void backtracking(int num)
+{
+  if (num == m)
+  {
+    for (int i = 0; i < num; i++)
+      cout << arr[i] << " ";
+    cout << "\n";
+    return;
+  }
+  for (int i = 1; i <= n; i++)
+  {
+    arr[num] = i;
+    backtracking(num + 1);
+  }
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n >> m;
+
+  backtracking(0);
+
+  return 0;
+}
