ba743d473266d33b570a584f530ccc3ccbfb2b19
hajoon <87131215+hazzun@users.noreply.github.com>
2023-10-10 16:09:32 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,42 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int n, m;
 int arr[10];
-bool isused[10];
-
-void backtracking(int num)
-{
-  if (num == m)
-  {
-    for (int i = 0; i < num; i++)
-      cout << arr[i] << " ";
-    cout << "\n";
-    return;
-  }
-  for (int i = 1; i <= n; i++)
-  {
-    if (!isused[i])
-    {
-      arr[num] = i;
-      if (num > 0 && arr[num - 1] >= arr[num])
-        continue;
-      isused[i] = 1;
-      backtracking(num + 1);
-      isused[i] = 0;
-    }
-  }
-}
+int check[10];
 
 int main()
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
   cin >> n >> m;
+  for (int i = 1; i <= n; i++)
+  {
+    arr[i - 1] = i;
+    if (i > m)
+      check[i - 1] = 1;
+  }
 
-  backtracking(0);
-
-  return 0;
+  do
+  {
+    for (int i = 0; i < n; i++)
+    {
+      if (check[i] == 0)
+        cout << i + 1 << " ";
+    }
+    cout << "\n";
+  } while (next_permutation(check, check + n));
 }
